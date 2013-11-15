38db8cc51e07bdd7ee1652aec656e0b7a914a917
Ricacom <ricacom@gmail.com>
2013-11-15 12:39:43 -0200

    Update files bootstrap footer head and first page input data

--- includes/msgHelper.php
+++ includes/msgHelper.php
@@ -4,13 +4,28 @@
 //alert Bootstrap ERROR
 function msg_error($title, $msg, $obs = ''){
     
-    $msgReturn  = "<div class='alert alert-error'>";
+    $msgReturn  = "<div class='alert alert-danger'>";
+    
     $msgReturn .= "<button type='button' class='close' data-dismiss='alert'>&times;</button>";
     $msgReturn .= "<h4>" .$title ."</h4>";
     $msgReturn .= $msg;
     $msgReturn .= "<br>" .$obs;
     $msgReturn .= "</div>";
+  
+    return $msgReturn;
+}
+
+
+// alert de sucess
+
+function msg_success($title, $msg, $obs = ''){
     
+    $msgReturn  = "<div class='alert alert-success'>";
+    $msgReturn .= "<button type='button' class='close' data-dismiss='alert'>&times;</button>";
+    $msgReturn .= "<h4>" .$title ."</h4>";
+    $msgReturn .= $msg;
+    $msgReturn .= "<br>" .$obs;
+    $msgReturn .= "</div>";
     
     return $msgReturn;
 }
